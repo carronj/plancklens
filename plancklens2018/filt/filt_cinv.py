b31777c4615d788cb30e5deebffa41f3546b7c09
jcarron <j.carron@sussex.ac.uk>
2018-10-28 12:25:23 +0100

    minor, various

--- plancklens2018/filt/filt_cinv.py
+++ plancklens2018/filt/filt_cinv.py
@@ -2,7 +2,8 @@
 
 Todo:
     * tests
-    * cinv_p, cinv_t, cinv_t_vmap
+    * cinv_p, cinv_t_vmap
+    * drop _vmap and adapt cinv_t cinv_p (calc mask and what else?)
     * doc
     * paths format
 """
@@ -29,7 +30,12 @@ from plancklens2018.qcinv import multigrid, cd_solve
 class library_cinv_sepTP(filt_simple.library_sepTP):
     """Library to perform inverse variance filtering on the sim_lib library.
 
-    The lib_dir path is setup at instantiation and will contain the ivfs filtered map as healpix alm fits files.
+    Args:
+        lib_dir (str):
+        sim_lib:
+        cinv_t:
+        cinv_p:
+        soltn_lib (optional):
     """
 
     def __init__(self, lib_dir, sim_lib, cinv_t, cinv_p, soltn_lib=None):
@@ -121,6 +127,114 @@ class cinv(object):
         assert len(ret) > lmax, (len(ret), lmax)
         return ret[:lmax + 1]
 
+
+
+class cinv_t(cinv):
+    def __init__(self, lib_dir, lmax, nside, cl, transf, ninv,
+                 marge_monopole=True, marge_dipole=True, marge_maps=(), pcf='default', chain_descr=None):
+        #FIXME: do I need the mask on disk? Dont think so
+        """
+
+        Args:
+            ninv: inverse pixel variance map. Must be a list of paths or 1d-arrays with the right size.
+                  Can contain a mask but only homogeneous noise. Use cinv_t_vmap for inhomog. variance maps.
+        """
+        assert lib_dir is not None and lmax >= 1024 and nside >= 512, (lib_dir, lmax, nside)
+        assert isinstance(ninv, list)
+        super(cinv_t, self).__init__(lib_dir, lmax)
+
+
+        self.nside = nside
+        self.cl = cl
+        self.transf = transf
+        self.ninv = ninv
+        self.marge_monopole = marge_monopole
+        self.marge_dipole = marge_dipole
+        self.marge_maps = marge_maps
+
+        pcf = lib_dir + "/dense.pk" if pcf == 'default' else '' # Dense matrices will be cached there.
+        if chain_descr is None : chain_descr = \
+            [[3, ["split(dense(" + pcf + "), 64, diag_cl)"], 256, 128, 3, 0.0, cd_solve.tr_cg, cd_solve.cache_mem()],
+             [2, ["split(stage(3),  256, diag_cl)"], 512, 256, 3, 0.0, cd_solve.tr_cg, cd_solve.cache_mem()],
+             [1, ["split(stage(2),  512, diag_cl)"], 1024, 512, 3, 0.0, cd_solve.tr_cg, cd_solve.cache_mem()],
+             [0, ["split(stage(1), 1024, diag_cl)"], lmax, nside, np.inf, 1.0e-5, cd_solve.tr_cg, cd_solve.cache_mem()]]
+
+        n_inv_filt = util.jit(opfilt_tt.alm_filter_ninv, ninv, transf[0:lmax + 1],
+                        marge_monopole=marge_monopole, marge_dipole=marge_dipole, marge_maps=marge_maps)
+        self.chain = util.jit(multigrid.multigrid_chain, opfilt_tt, chain_descr, cl, n_inv_filt)
+        #opfilt, chain_descr, s_cls, n_inv_filt, debug_log_prefix=None, plogdepth=0
+        if mpi.rank == 0:
+            if not os.path.exists(lib_dir):
+                os.makedirs(lib_dir)
+
+            if not os.path.exists(lib_dir + "/filt_hash.pk"):
+                pk.dump(self.hashdict(), open(lib_dir + "/filt_hash.pk", 'w'))
+
+            if not os.path.exists(self.lib_dir + "/ftl.dat"):
+                np.savetxt(self.lib_dir + "/ftl.dat", self._calc_ftl())
+
+            if not os.path.exists(self.lib_dir + "/tal.dat"):
+                np.savetxt(self.lib_dir + "/tal.dat",  self._calc_tal())
+
+            if not os.path.exists(self.lib_dir + "/fmask.fits.gz"):
+                hp.write_map(self.lib_dir + "/fmask.fits.gz", self._calc_mask())
+
+        mpi.barrier()
+        utils.hash_check(pk.load(open(lib_dir + "/filt_hash.pk", 'r')), self.hashdict())
+
+    def _ninv_hash(self):
+        ret = []
+        for ninv_comp in self.ninv:
+            if isinstance(ninv_comp, np.ndarray) and ninv_comp.size > 1:
+                ret.append(utils.clhash(ninv_comp))
+            else:
+                ret.append(ninv_comp)
+        return ret
+
+    def _calc_ftl(self):
+        ninv = self.chain.n_inv_filt.n_inv
+        npix = len(ninv[:])
+        NlevT_uKamin = np.sqrt(4. * np.pi / npix / np.sum(ninv) * len(np.where(ninv != 0.0)[0])) * 180. * 60. / np.pi
+        print("cinv_t::noiseT_uk_arcmin = %.3f"%NlevT_uKamin)
+
+        s_cls = self.chain.s_cls
+        b_transf = self.chain.n_inv_filt.b_transf
+
+        if s_cls['tt'][0] == 0.: assert self.chain.n_inv_filt.marge_monopole
+        if s_cls['tt'][1] == 0.: assert self.chain.n_inv_filt.marge_dipole
+
+        ftl = utils.cli(s_cls['tt'][0:self.lmax + 1] + (NlevT_uKamin * np.pi / 180. / 60.) ** 2 / b_transf[0:self.lmax + 1] ** 2)
+        if self.chain.n_inv_filt.marge_monopole: ftl[0] = 0.0
+        if self.chain.n_inv_filt.marge_dipole: ftl[1] = 0.0
+
+        return ftl
+
+    def _calc_tal(self):
+        return utils.cli(self.transf)
+
+    def _calc_mask(self):
+        ninv = self.chain.n_inv_filt.n_inv
+        assert hp.npix2nside(len(ninv)) == self.nside
+        return np.where(ninv > 0, 1., 0.)
+
+    def hashdict(self):
+        return {'lmax': self.lmax,
+                'nside': self.nside,
+                'cltt': utils.clhash(self.cl['tt'][:self.lmax + 1]),
+                'transf': utils.clhash(self.transf[:self.lmax + 1]),
+                'ninv': self._ninv_hash(),
+                'marge_monopole': self.marge_monopole,
+                'marge_dipole': self.marge_dipole,
+                'marge_maps': self.marge_maps}
+
+    def apply_ivf(self, tmap, soltn=None):
+        if soltn is None:
+            talm = np.zeros(util_alm.lmax2nlm(self.lmax), dtype=np.complex)
+        else:
+            talm = soltn.copy()
+        self.chain.solve(talm, tmap)
+        return talm
+
 class cinv_p_vmap(cinv):
     def __init__(self, lib_dir, lmax, nside, cl, transf, mask_list, ninv_vmaps, marge_maps=(), pcf='default'):
         assert lmax >= 1024 and nside >= 512, (lmax, nside)
@@ -200,24 +314,24 @@ class cinv_p_vmap(cinv):
         if len(self.chain.n_inv_filt.n_inv) == 1:
             ninv = self.chain.n_inv_filt.n_inv[0]
             npix = len(ninv)
-            noiseP_uK_arcmin = np.sqrt(
+            NlevP_uKamin = np.sqrt(
                 4. * np.pi / npix / np.sum(ninv) * len(np.where(ninv != 0.0)[0])) * 180. * 60. / np.pi
         else:
             assert len(self.chain.n_inv_filt.n_inv) == 3
             ninv = self.chain.n_inv_filt.n_inv
-            noiseP_uK_arcmin = 0.5 * np.sqrt(
+            NlevP_uKamin = 0.5 * np.sqrt(
                 4. * np.pi / len(ninv[0]) / np.sum(ninv[0]) * len(np.where(ninv[0] != 0.0)[0])) * 180. * 60. / np.pi
-            noiseP_uK_arcmin += 0.5 * np.sqrt(
+            NlevP_uKamin += 0.5 * np.sqrt(
                 4. * np.pi / len(ninv[2]) / np.sum(ninv[2]) * len(np.where(ninv[2] != 0.0)[0])) * 180. * 60. / np.pi
-        print("cinv_p_vmap::calc_febl. noiseP_uk_arcmin = %.2f"%noiseP_uK_arcmin)
+        print("cinv_p_vmap::calc_febl. noiseP_uk_arcmin = %.2f"%NlevP_uKamin)
 
         s_cls = self.chain.s_cls
         b_transf = self.chain.n_inv_filt.b_transf[:self.lmax + 1]
 
-        fel = 1.0 / (s_cls['ee'][:self.lmax + 1] + (noiseP_uK_arcmin * np.pi / 180. / 60.) ** 2 / b_transf ** 2)
-        fel[:2] = 0.0
+        fel = 1.0 / (s_cls['ee'][:self.lmax + 1] + (NlevP_uKamin * np.pi / 180. / 60.) ** 2 / b_transf ** 2)
+        fbl = 1.0 / (s_cls['bb'][:self.lmax + 1] + (NlevP_uKamin * np.pi / 180. / 60.) ** 2 / b_transf ** 2)
 
-        fbl = 1.0 / (s_cls['bb'][:self.lmax + 1] + (noiseP_uK_arcmin * np.pi / 180. / 60.) ** 2 / b_transf ** 2)
+        fel[:2] = 0.0
         fbl[:2] = 0.0
 
         return fel, fbl
@@ -226,10 +340,8 @@ class cinv_p_vmap(cinv):
         return utils.cli(self.transf[:self.lmax + 1])
 
     def calc_mask(self):
-        """ returns the inverse mask map / max(inverse mask map) """
         n_inv = self.mask_list
         if isinstance(n_inv, list):
-            # The following line does nothing is if the first element of n_inv is not a string.
             n_inv_prod = util.load_map(n_inv[0])
             if len(n_inv) > 1:
                 for n in n_inv[1:]:
@@ -237,7 +349,6 @@ class cinv_p_vmap(cinv):
             n_inv = n_inv_prod
         else:
             n_inv = util.load_map(n_inv)
-            # This changes the attributes of qcinv to the full mask.
         return n_inv / np.max(n_inv)
 
     def apply_ivf(self, pmap, soltn=None):
