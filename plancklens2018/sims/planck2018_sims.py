2346e9651078dc4e3933989186ce7fa01545bc46
jcarron <j.carron@sussex.ac.uk>
2018-12-14 10:42:31 +0000

    sims module (incomplete)

---
+++ plancklens2018/sims/planck2018_sims.py
@@ -0,0 +1,16 @@
+"""Planck 2018 release simulation libraries.
+
+"""
+
+class smica_dx12:
+    def __init__(self):
+        pass
+
+    def hashdict(self):
+        return {'sim_lib': 'planck2018_smica_full'}
+
+    def get_sim_tmap(self):
+        assert 0
+
+    def get_sim_pmap(self):
+        assert 0
