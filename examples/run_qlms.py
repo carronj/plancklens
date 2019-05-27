26ae6a943fe2a7ec87af473b078383348814a71e
jcarron <j.carron@sussex.ac.uk>
2019-05-27 16:03:22 +0200

    qcl script

--- examples/run_qlms.py
+++ examples/run_qlms.py
@@ -8,6 +8,10 @@ parser.add_argument('-imin', dest='imin', default=-1, type=int, help='starting i
 parser.add_argument('-imax', dest='imax', default=-2, type=int, help='last index')
 parser.add_argument('-k', dest='k', action='store', default=['p'], nargs='+',
                     help='QE keys (NB: both gradient and curl are calculated at the same time)')
+parser.add_argument('-kA', dest='kA', action='store', default=['p'], nargs='+',
+                    help='QE spectra keys (left leg)')
+parser.add_argument('-kB', dest='kB', action='store', default=['p'], nargs='+',
+                    help='QE spectra keys (right leg)')
 parser.add_argument('-ivt', dest='ivt', action='store_true', help='do T. filtering')
 parser.add_argument('-ivp', dest='ivp', action='store_true', help='do P. filtering')
 parser.add_argument('-dd', dest='dd', action='store_true', help='perform dd qlms library QEs')
@@ -20,9 +24,9 @@ par = imp.load_source('run_qlms_parfile', args.parfile[0])
 #--- filtering
 jobs = []
 if args.ivt:
-    jobs +=  [ (idx, 't') for idx in range(args.imin, args.imax + 1)]
+    jobs += [(idx, 't') for idx in range(args.imin, args.imax + 1)]
 if args.ivp:
-    jobs += [ (idx, 'p') for idx in range(args.imin, args.imax + 1)]
+    jobs += [(idx, 'p') for idx in range(args.imin, args.imax + 1)]
 
 for i, (idx, lab) in enumerate(jobs[mpi.rank::mpi.size]):
     print('rank %s filtering sim %s %s, job %s in %s'%(mpi.rank, idx, lab, i, len(jobs)))
@@ -42,6 +46,20 @@ for qlib in qlibs:
 for i, (qlib, idx, k) in enumerate(jobs[mpi.rank::mpi.size]):
     print('rank %s doing QE sim %s %s, qlm_lib %s, job %s in %s' % (mpi.rank, idx, k, qlib.lib_dir, i, len(jobs)))
     qlib.get_sim_qlm(k, idx)
+
+#--- QE power spectra #FIXME: mfs
+qlibs = [par.qcls_dd] * args.dd + [par.qcls_ds] * args.ds +  [par.qcls_ss] * args.ss
+jobs = []
+for qlib in qlibs:
+    for kA in args.kA:
+        for kB in args.kB:
+            for idx in range(args.imin, args.imax):
+                if idx not in qlib.mc_sims_mf:
+                    jobs.append((qlib, idx, kA, kB))
+
+for i, (qlib, idx, kA, kB) in enumerate(jobs[mpi.rank::mpi.size]):
+    print('rank %s doing QE spectra sim %s %s, qcl_lib %s, job %s in %s' % (mpi.rank, idx, k, qlib.lib_dir, i, len(jobs)))
+    qlib.get_sim_qcl(kA, idx, k2=kB)
 
 mpi.barrier()
 mpi.finalize()
