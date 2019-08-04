d3eeb6ca00279981784155155e9de45a851736d6
jcarron <j.carron@sussex.ac.uk>
2019-08-04 23:56:45 +0200

    small edits

--- docs/conf.py
+++ docs/conf.py
@@ -47,8 +47,6 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.githubpages']
 
 
-html_theme = "sphinx_rtd_theme"
-
 # mathjax settings
 mathjax_config ={
     'extensions': ['tex2jax.js'],
@@ -121,7 +119,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
--- docs/index.rst
+++ docs/index.rst
@@ -6,6 +6,8 @@
 Welcome to plancklens documentation!
 ====================================
 
+Is this working at all?
+
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
