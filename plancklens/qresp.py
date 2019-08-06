85b77cbfc5809214b274cf627f28f09f4587fabd
jcarron <j.carron@sussex.ac.uk>
2019-08-06 15:35:24 +0200

    get_qe bug fix

--- plancklens/qresp.py
+++ plancklens/qresp.py
@@ -44,7 +44,7 @@ def get_qes(qe_key, lmax, cls_weight, lmax2=None):
                     lega = uqe.qeleg(s_left, s_left, 0.5 *(1. + (s_left == 0)) * np.ones(lmax + 1, dtype=float))
                     legb = uqe.qeleg(sin, sout + s_qe, 0.5 * (1. + (sin == 0)) * 2 * cl_sosi)
                     qes.append(uqe.qe(lega, legb, cL_out))
-        if len(qe_key) == 1:
+        if len(qe_key) == 1 or qe_key[1:] in ['tt', '_p']:
             return uqe.qe_simplify(qes)
         elif qe_key[1:] in ['te', 'et', 'tb', 'bt', 'ee', 'eb', 'be', 'bb']:
             return uqe.qe_simplify(uqe.qe_proj(qes, qe_key[1], qe_key[2]))
