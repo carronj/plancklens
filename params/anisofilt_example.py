5b431b4390ff74dbc06a38128b78a861dab1c58d
jcarron <to.jcarron@gmail.com>
2020-11-30 20:46:47 +0100

    lmin cut in anisofilt example

--- params/anisofilt_example.py
+++ params/anisofilt_example.py
@@ -91,7 +91,11 @@ cinv_t = filt_cinv.cinv_t(libdir_cinvt, lmax_ivf,nside, cl_len, transf, ninv_t,
 ninv_p = [[np.array([3. / nlev_p ** 2])] + maskpaths]
 cinv_p = filt_cinv.cinv_p(libdir_cinvp, lmax_ivf, nside, cl_len, transf, ninv_p)
 
-ivfs    = filt_cinv.library_cinv_sepTP(libdir_ivfs, sims, cinv_t, cinv_p, cl_len)
+ivfs_raw    = filt_cinv.library_cinv_sepTP(libdir_ivfs, sims, cinv_t, cinv_p, cl_len)
+ftl = np.ones(lmax_ivf + 1, dtype=float) * (np.arange(lmax_ivf + 1) >= lmin_ivf) # rescaling or cuts. Here just a lmin cut
+fel = np.ones(lmax_ivf + 1, dtype=float) * (np.arange(lmax_ivf + 1) >= lmin_ivf)
+fbl = np.ones(lmax_ivf + 1, dtype=float) * (np.arange(lmax_ivf + 1) >= lmin_ivf)
+ivfs   = filt_util.library_ftl(ivfs_raw, lmax_ivf, ftl, fel, fbl)
 
 #: Inverse-variance filtering instance. Here a trivial isotropic inverse variance weighting.
 
