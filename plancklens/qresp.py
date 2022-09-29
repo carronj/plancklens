caa7e12e90a0a2a48f7b92a0dc225899c8be0da4
jcarron <to.jcarron@gmail.com>
2022-09-29 19:40:49 +0200

    ktt

--- plancklens/qresp.py
+++ plancklens/qresp.py
@@ -170,7 +170,7 @@ def qe_spin_data(qe_key):
     """
     if qe_key in ['ntt']:
         return 0, 'G', [0], 'n'
-    qes = get_qes(qe_key, 3, {k:np.array([1.]) for k in ['tt', 'te', 'ee', 'bb']})
+    qes = get_qes(qe_key, 10, {k:np.array([1.]) for k in ['tt', 'te', 'ee', 'bb']}) #Hack
     spins_out = [qe.leg_a.spin_ou + qe.leg_b.spin_ou for qe in qes]
     spins_in = np.unique(np.abs([qe.leg_a.spin_in for qe in qes] + [qe.leg_b.spin_in for qe in qes]))
     assert len(np.unique(spins_out)) == 1, spins_out
