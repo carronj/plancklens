d0dba9ebcc5068ee7ce4d278f84a0b70b9491a1d
Sebastian-Belkner <sebastian.belkner@unige.ch>
2023-02-16 22:02:11 +0100

    catch mpi import when run from jupyter

--- plancklens/helpers/mpi.py
+++ plancklens/helpers/mpi.py
@@ -7,10 +7,22 @@ import os
 
 verbose = False
 
+def is_notebook() -> bool:
+    try:
+        shell = get_ipython().__class__.__name__
+        if shell == 'ZMQInteractiveShell':
+            return True   # Jupyter notebook or qtconsole
+        elif shell == 'TerminalInteractiveShell':
+            return False  # Terminal running IPython
+        else:
+            return False  # Other type (?)
+    except NameError:
+        return False
+    
 has_key = lambda key : key in os.environ.keys()
 cond4mpi4py = not has_key('NERSC_HOST') or (has_key('NERSC_HOST') and has_key('SLURM_SUBMIT_DIR'))
 
-if cond4mpi4py:
+if not is_notebook() and cond4mpi4py:
     try:
         from mpi4py import MPI
 
