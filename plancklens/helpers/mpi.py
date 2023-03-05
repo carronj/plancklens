3c8bc71d6b9f1fb86a591b100b272d6864c689ec
Sebastian-Belkner <sebastian.belkner@unige.ch>
2023-03-05 21:23:53 +0100

    add switch

--- plancklens/helpers/mpi.py
+++ plancklens/helpers/mpi.py
@@ -2,31 +2,49 @@
 
 """
 
-from __future__ import print_function
 import os
 
 verbose = False
-
 has_key = lambda key : key in os.environ.keys()
-cond4mpi4py = not has_key('NERSC_HOST') or (has_key('NERSC_HOST') and has_key('SLURM_SUBMIT_DIR'))
 
-if cond4mpi4py:
+cond4mpi4py = not has_key('NERSC_HOST') or (has_key('NERSC_HOST') and has_key('SLURM_SUBMIT_DIR'))
+use =  has_key('USE_PLANCKLENS_MPI') and os.environ['USE_PLANCKLENS_MPI']
+
+if cond4mpi4py and use:
     try:
         from mpi4py import MPI
 
         rank = MPI.COMM_WORLD.Get_rank()
         size = MPI.COMM_WORLD.Get_size()
         barrier = MPI.COMM_WORLD.Barrier
+        ANY_SOURCE = MPI.ANY_SOURCE
+        send = MPI.COMM_WORLD.send
+        receive = MPI.COMM_WORLD.recv
+        bcast = MPI.COMM_WORLD.bcast
+        status = MPI.Status
         finalize = MPI.Finalize
+
         if verbose: print('mpi.py : setup OK, rank %s in %s' % (rank, size))
     except:
         rank = 0
         size = 1
         barrier = lambda: -1
+        bcast = lambda _: 0
         finalize = lambda: -1
-        if verbose: print('mpi.py: unable to import mpi4py\n')
+        bcast = lambda _: 0
+        send = lambda _, dest: 0
+        receive = lambda _, source: 0
+        ANY_SOURCE = 0
+
+        if verbose and use: print('mpi.py: unable to import mpi4py\n')
 else:
     rank = 0
     size = 1
     barrier = lambda: -1
+    bcast = lambda _: 0
+    send = lambda _, dest: 0
+    receive = lambda _, source: 0
     finalize = lambda: -1
+    ANY_SOURCE = 0
+    
+    if not use: print('mpi.py: Plancklens.mpi disabled as per environ variable \n')
