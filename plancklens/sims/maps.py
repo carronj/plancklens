49428835427651c298e3a4c1e821d46a57f00e56
jcarron <to.jcarron@gmail.com>
2023-05-03 08:41:52 +0200

    unnecessary assert

--- plancklens/sims/maps.py
+++ plancklens/sims/maps.py
@@ -201,7 +201,8 @@ class cmb_maps_harmonicspace(object):
         self.phas = noise_phas
         self.nside = nside
 
-        assert self.sims_cmb_len.lmax == self.phas.lmax, f"Lmax of lensed CMB and of noise phases should match, here {self.sims_cmb_len.lmax} and {self.phas.lmax}"
+        if hasattr(sims_cmb_len, 'lmax'):
+            assert self.sims_cmb_len.lmax == self.phas.lmax, f"Lmax of lensed CMB and of noise phases should match, here {self.sims_cmb_len.lmax} and {self.phas.lmax}"
 
         if lib_dir is not None:
             fn_hash = os.path.join(lib_dir, 'sim_hash.pk')
